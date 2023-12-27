015969baddd7a45c338fe16758c19493cf15e410
FcoCervantesRdz <fco.cervantesrdz@gmail.com>
2023-12-27 14:10:18 -0600

    Day 1. Basic input/output strings

---
+++ Day_001-Beginner-Working_with_variables/001-Band_name_generator.py
@@ -0,0 +1,4 @@
+print("Hi, this is a band name generator. If you want to go ahead please answer next questions:")
+city = input("What's the city where you drew up in?: ")
+pet_name = input("What's the name of one of your pets?: ")
+print("Your band name is '"+city+" "+pet_name+"'")
